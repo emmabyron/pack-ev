baa919f35ccb440c6bef68b0a613591cf4ae7685
Emma Byron <emmabyron@Michelles-MacBook-Air.local>
2020-09-20 11:36:19 -0400

    Added basics.

--- znr-draftbooster.py
+++ znr-draftbooster.py
@@ -2,7 +2,7 @@ import requests
 
 minPrice = 0.5
 
-response = str(requests.get("https://api.scryfall.com/cards/search?q=s%3Aznr+is%3Abooster+r%3Ac").json())
+response = str(requests.get("https://api.scryfall.com/cards/search?q=s%3Aznr+is%3Abooster+r%3Ac+-t%3A\"basic+land\"").json())
 
 commons = []
 
@@ -19,7 +19,7 @@ while response.find("'name'") != -1:
     if commons[-1][1] < minPrice:
         commons[-1][1] = 0
 
-response = str(requests.get("https://api.scryfall.com/cards/search?q=s%3Aznr+is%3Abooster+r%3Au").json())
+response = str(requests.get("https://api.scryfall.com/cards/search?q=s%3Aznr+is%3Abooster+r%3Au+-t%3A\"basic+land\"").json())
 
 uncommons = []
 
@@ -36,7 +36,7 @@ while response.find("'name'") != -1:
     if uncommons[-1][1] < minPrice:
         uncommons[-1][1] = 0
 
-response = str(requests.get("https://api.scryfall.com/cards/search?q=s%3Aznr+is%3Abooster+r%3Ar").json())
+response = str(requests.get("https://api.scryfall.com/cards/search?q=s%3Aznr+is%3Abooster+r%3Ar+-t%3A\"basic+land\"").json())
 
 rares = []
 
@@ -53,7 +53,7 @@ while response.find("'name'") != -1:
     if rares[-1][1] < minPrice:
         rares[-1][1] = 0
 
-response = str(requests.get("https://api.scryfall.com/cards/search?q=s%3Aznr+is%3Abooster+r%3Am").json())
+response = str(requests.get("https://api.scryfall.com/cards/search?q=s%3Aznr+is%3Abooster+r%3Am+-t%3A\"basic+land\"").json())
 
 mythics = []
 
@@ -69,6 +69,23 @@ while response.find("'name'") != -1:
     
     if mythics[-1][1] < minPrice:
         mythics[-1][1] = 0
+
+response = str(requests.get("https://api.scryfall.com/cards/search?q=s%3Aznr+is%3Abooster+t%3A\"basic+land\"").json())
+
+basics = []
+
+while response.find("'name'") != -1:
+    response = response[response.find("'name'") + 9:]
+
+    basics.append([response[:response.find("'lang'") - 3], response[response.find("'usd'") + 8:response.find("'usd_foil'") - 3]])
+
+    try:
+        basics[-1][1] = float(basics[-1][1])
+    except:
+        basics[-1][1] = 0
+    
+    if basics[-1][1] < minPrice:
+        basics[-1][1] = 0
 
 for index in range(len(commons)):
     if commons[index][0].find("//") != -1:
@@ -98,7 +115,7 @@ for index in range(len(mythics)):
 
 mythics = [item for item in mythics if item != "remove"]
 
-costs = [0, 0, 0, 0]
+costs = [0, 0, 0, 0, 0]
 
 for item in commons:
     costs[0] += item[1]
@@ -108,6 +125,8 @@ for item in rares:
     costs[2] += item[1]
 for item in mythics:
     costs[3] += item[1]
+for item in basics:
+    costs[4] += item[1]
 
 for index in range(4):
     costs[index] = round(costs[index], 2)
@@ -118,6 +137,7 @@ ev += 11 * (costs[0] / len(commons))
 ev += 3 * (costs[1] / len(uncommons))
 ev += (6.4 / 7.4) * (costs[2] / len(rares))
 ev += (1 / 7.4) * (costs[3] / len(mythics))
+ev += costs[4] / len(basics)
 
 ev = round(ev, 2)
 
